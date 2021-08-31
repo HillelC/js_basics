3e4afa31b714efd61a408ba86317d6bd80f63eef
HillelC <89578016+HillelC@users.noreply.github.com>
2021-08-31 23:51:25 +0300

    gussCity.js

---
+++ gussCity.js
@@ -0,0 +1,16 @@
+function guessCity(capital, coastal, famous, ancient) {
+  
+    if (capital===true && coastal===false && famous===true && ancient===true ) {
+        return "Jerusalem";
+    } else if (capital ===false && coastal===true && famous===true && ancient===false) {
+        return "Tel-aviv";
+    } else if (capital===false && coastal===true && famous===false && ancient===true) {
+        return "Arce";
+    } else if (capital===false && coastal===false && famous===false && ancient===true) {
+        return "Katzrin";
+    } else if (capital===false && coastal===true && famous===false && ancient===false) {
+        return "Zikim";
+    } else if (capital===false && coastal===false && famous===false && ancient===false) {
+        return "musmus";
+    }
+}
